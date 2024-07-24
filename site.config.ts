51dd38441362bbfbf20887a5d2f7ea868765736f
llsewa <111433060+llsewa@users.noreply.github.com>
2024-07-24 14:46:21 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,10 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
+  WeChat: '18211528313',
   github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'Siying Wu',
+  bilibili: 'llsewa',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
